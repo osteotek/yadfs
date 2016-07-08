ed1e7290a119d20eaa5f1c5bd467943472249b7b
Yuri Fedoseev <y.fedoseev@innopolis.ru>
2016-07-08 18:43:49 +0300

    stub for NS

--- name_server.py
+++ name_server.py
@@ -1,2 +1,36 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import sys
+
+
 class NameServer:
-    pass
+    def get_cs(self):
+        return "cs-1"
+
+    def create_file(self, data):
+        return "ok"
+
+    def delete_file(self, path):
+        return "ok"
+
+    def locate_file(self, path):
+        return "ok"
+
+    def make_directory(self, path):
+        return "ok"
+
+    def list_directory(self, path):
+        return "ok"
+
+
+# ars: host and port
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("You have to specify host and port!")
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+
+    server = SimpleXMLRPCServer((host, port))
+    server.register_introspection_functions()
+    server.register_instance(NameServer())
+    server.serve_forever()
