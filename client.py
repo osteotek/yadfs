829eeae7cb63ab9275bafa6199015fb69e6f2504
Arthur Tazhitdinov <lisnake@gmail.com>
2016-07-08 20:03:21 +0300

    functions for splitting and combining files

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from xmlrpc.client import ServerProxy
 
@@ -7,9 +8,35 @@ class Client:
         self.ns = ServerProxy(addr)
 
     def start(self):
-        print("send test request to ns")
-        r = self.ns.list_directory("some_path")
+        print('send test request to ns')
+        r = self.ns.list_directory('/')
         print(r)
+
+    def splitfile(self, filename, chunksize=1024):
+        if not os.path.isfile(filename):
+            raise IOError('No such file as: {0}'.format(filename))
+
+        filesize = os.stat(filename).st_size
+
+        with open(filename, 'rb') as fr:
+            n_chunks = filesize // chunksize
+            chunks = []
+            print('splitfile: No of chunks required: {0}'.format(str(n_chunks + 1)))
+            for i in range(n_chunks + 1):
+                data = fr.read(chunksize)
+                chunks.append(data)
+                #with open(filename + "_{0}".format(str(i)), 'xb') as fw:
+                #    fw.write(data)
+            return chunks
+
+    def combinefile(self, filename, chunks):
+        if os.path.isfile(filename):
+            raise IOError('Such file already exists: {0}'.format(filename))
+
+        with open(filename, 'xb') as fw:
+            for chunk in chunks:
+                fw.write(chunk)
+
 
 # first arg - NS address - http://localhost:888
 if __name__ == '__main__':
