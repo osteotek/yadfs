13543d01467c3c68f6460bb14e8ca1a67c3ae472
Yuri Fedoseev <y.fedoseev@innopolis.ru>
2016-07-08 19:05:19 +0300

    Virtual file

--- name_server.py
+++ name_server.py
@@ -1,12 +1,40 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import sys
+from enum import Enum
+
+
+class NodeType(Enum):
+    file = 1
+    directory = 2
+
+
+class FileNode:
+    def __init__(self, name, node_type):
+        self.name = name
+        self.parent = None
+        self.children = []
+        self.type = node_type
+
+        # list of file chunks, empty for the directory
+        self.chunks = {}
+
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+    def find_path(self):
+        pass
 
 
 class NameServer:
+    def __init__(self):
+        self.root = FileNode('root', NodeType.directory)
+
     def get_cs(self):
         return "cs-1"
 
     def create_file(self, data):
+        file = FileNode(data['name'], NodeType.file)
         return "ok"
 
     def delete_file(self, path):
